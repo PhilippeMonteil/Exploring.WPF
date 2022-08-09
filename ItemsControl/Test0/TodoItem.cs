31dd153359375d123669e9b74cd14926b2d9e4d3
Philippe <Philippe@DESKTOP-FCE7OUJ>
2022-08-09 17:17:36 +0200

    update

---
+++ ItemsControl/Test0/TodoItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test0
+{
+
+    public class TodoItem
+    {
+        // get requis pour le binding
+        public string Title { get; set; }
+        public int Completion { get; set; }
+
+        public TodoItem(string Title, int Completion)
+        {
+            this.Title = Title;
+            this.Completion = Completion;
+        }
+
+    }
+
+}
---
+++ ItemsControl/Test0/TodoItemListTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test0
+{
+
+    public class TodoItemListTest : List<TodoItem>
+    {
+
+        public TodoItemListTest()
+        {
+            for (int i=0;i < 100; i++)
+            {
+                Add(new TodoItem($"TestItem0[{i}]", 90));
+            }
+        }
+
+    }
+
+}
