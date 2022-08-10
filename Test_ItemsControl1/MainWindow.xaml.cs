d39d4b106cc1fe05a9040c99633ae700f94c0096
Philippe <Philippe@DESKTOP-FCE7OUJ>
2022-08-10 16:09:21 +0200

    cleanup

--- Test_ItemsControl1/MainWindow.xaml.cs
+++ Test_ItemsControl1/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ItemsControlLib;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,7 @@ namespace Test_ItemsControl1
         public MainWindow()
         {
             InitializeComponent();
+            this.DataContext = new TodoItemListTest();
         }
     }
 }
