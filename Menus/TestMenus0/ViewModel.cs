fb3cc550a4c509d94b8f43efaf832cfea73cee5e
Philippe <Philippe@DESKTOP-FCE7OUJ>
2022-08-18 17:36:29 +0200

    update

---
+++ Menus/TestMenus0/ViewModel.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CommunityToolkit.Mvvm;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace TestMenus0
+{
+
+    internal class ViewModel : ObservableObject
+    {
+        public ViewModel()
+        {
+        }
+
+        RelayCommand<object> _relayCommand = new RelayCommand<object>(param =>
+        {
+            Debug.WriteLine($"_relayCommand param={param}");
+        },
+        param => true);
+
+        public RelayCommand<object> Command => _relayCommand;
+
+    }
+
+}
--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 # DrawingVisual.md
 
+# Menus
+
+
+
