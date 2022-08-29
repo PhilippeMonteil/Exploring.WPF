600193201453d1b2d7825e868209a02d645e5219
Philippe <Philippe@DESKTOP-FCE7OUJ>
2022-08-29 12:15:21 +0200

    TestResources

--- Resources/TestResources/Program.cs
+++ Resources/TestResources/Program.cs
@@ -1,10 +1,31 @@
-﻿namespace TestResources
+﻿
+using TestResources.Properties;
+
+namespace TestResources
 {
+
     internal class Program
     {
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            Console.WriteLine($"[{Thread.CurrentThread.ManagedThreadId}] Resource0.String1={Resource0.String1}");
+
+            Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("en");
+            Console.WriteLine($"[{Thread.CurrentThread.ManagedThreadId}] Resource0.String1={Resource0.String1}");
+
+            Task.Run(() =>
+            {
+                Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("fr");
+                Console.WriteLine($"[{Thread.CurrentThread.ManagedThreadId}] Resource0.String1={Resource0.String1}");
+            });
+
+            Console.WriteLine("...");
+            Console.ReadLine();
         }
+
     }
+
 }
