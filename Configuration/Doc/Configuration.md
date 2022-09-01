ca6e70f2ba6499b83900df92f822131e8084378f
Philippe <Philippe@DESKTOP-FCE7OUJ>
2022-09-01 10:02:17 +0200

    cleanup

--- Configuration/Doc/Configuration.md
+++ Configuration/Doc/Configuration.md
@@ -61,7 +61,24 @@
 
 ## Exemple : idem avec Hosting
 
-## Eample : with hosting and using the indexer API
+     {
+         using IHost host = Host.CreateDefaultBuilder(args).Build();
 
+         // Ask the service provider for the configuration abstraction.
+         IConfiguration config = host.Services.GetRequiredService<IConfiguration>();
+
+         // Get values from the config given their key and their target type.
+         int keyOneValue = config.GetValue<int>("KeyOne");
+         bool keyTwoValue = config.GetValue<bool>("KeyTwo");
+         string keyThreeNestedValue = config.GetValue<string>("KeyThree:Message");
+
+         // Write the values to the console.
+         Console.WriteLine($"KeyOne = {keyOneValue}");
+         Console.WriteLine($"KeyTwo = {keyTwoValue}");
+         Console.WriteLine($"KeyThree:Message = {keyThreeNestedValue}");
+
+     }
 
 ## [IConfiguration ](https://docs.microsoft.com/en-us/dotnet/api/microsoft.extensions.configuration.iconfiguration?view=dotnet-plat-ext-6.0)
+
+## [Configuration providers in .NET](https://docs.microsoft.com/en-us/dotnet/core/extensions/configuration-providers?source=recommendations)
--- Configuration/TestConfiguration/TestConfiguration.Program.cs
+++ Configuration/TestConfiguration/TestConfiguration.Program.cs
@@ -1,5 +1,7 @@
 ﻿
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace TestConfiguration
 {
@@ -10,7 +12,8 @@ namespace TestConfiguration
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
-            Test0();
+            //Test0();
+            Test1(args);
         }
 
         static void Test0()
@@ -28,6 +31,25 @@ namespace TestConfiguration
             Console.WriteLine($"KeyOne = {settings.KeyOne}");
             Console.WriteLine($"KeyTwo = {settings.KeyTwo}");
             Console.WriteLine($"KeyThree:Message = {settings.KeyThree.Message}");
+
+        }
+
+        static void Test1(string[] args)
+        {
+            using IHost host = Host.CreateDefaultBuilder(args).Build();
+
+            // Ask the service provider for the configuration abstraction.
+            IConfiguration config = host.Services.GetRequiredService<IConfiguration>();
+
+            // Get values from the config given their key and their target type.
+            int keyOneValue = config.GetValue<int>("KeyOne");
+            bool keyTwoValue = config.GetValue<bool>("KeyTwo");
+            string keyThreeNestedValue = config.GetValue<string>("KeyThree:Message");
+
+            // Write the values to the console.
+            Console.WriteLine($"KeyOne = {keyOneValue}");
+            Console.WriteLine($"KeyTwo = {keyTwoValue}");
+            Console.WriteLine($"KeyThree:Message = {keyThreeNestedValue}");
 
         }
 
