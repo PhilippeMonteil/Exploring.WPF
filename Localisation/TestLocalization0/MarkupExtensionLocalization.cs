44e124774988e6efdb693f27a80bacfc853278d9
Philippe <Philippe@DESKTOP-FCE7OUJ>
2022-09-02 18:00:54 +0200

    MarkupExtensionLocalization

---
+++ Localisation/TestLocalization0/MarkupExtensionLocalization.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+
+namespace TestLocalization0
+{
+
+    [MarkupExtensionReturnType(typeof(string))]
+    public class MarkupExtensionLocalization : MarkupExtension
+    {
+
+        public MarkupExtensionLocalization(string id)
+        {
+            this.ID = id;
+        }
+
+        [ConstructorArgument("id")]
+        public string ID { get; set; }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return $"ID={ID}";
+        }
+
+    }
+
+}
