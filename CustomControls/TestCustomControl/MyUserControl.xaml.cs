8c041e0cf0dbbfce8b90d294fcafc4c24c59cceb
Philippe <Philippe@DESKTOP-FCE7OUJ>
2022-09-10 19:19:23 +0200

    update

--- CustomControls/TestCustomControl/MyUserControl.xaml.cs
+++ CustomControls/TestCustomControl/MyUserControl.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,7 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Markup;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
@@ -15,14 +17,50 @@ using System.Windows.Shapes;
 
 namespace TestCustomControl
 {
+
     /// <summary>
     /// Interaction logic for MyUserControl.xaml
     /// </summary>
+    /// 
+    [ContentProperty(nameof(FileName))]
     public partial class MyUserControl : UserControl
     {
+        static DependencyProperty FileNameProperty = DependencyProperty.Register(nameof(FileName),
+                                                        typeof(string),
+                                                        typeof(MyUserControl),
+                                                        new PropertyMetadata(null, fileName_ChangedCallback));
+
+        static void fileName_ChangedCallback(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            (d as MyUserControl)?._fileName_ChangedCallback(e);
+        }
+
+        void _fileName_ChangedCallback(DependencyPropertyChangedEventArgs e)
+        {
+            // this.theTextBox.Text = e.NewValue as string;
+        }
+
         public MyUserControl()
         {
             InitializeComponent();
         }
+
+        public string FileName
+        {
+            get
+            {
+                return this.GetValue(FileNameProperty) as string;
+            }
+            set
+            {
+                this.SetValue(FileNameProperty, value);
+            }
+        }
+
+        private void theButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.FileName += "+";
+        }
     }
+
 }
