a40fe150ed7b131a808bf31e6eeea84aaaeb1494
Philippe <Philippe@DESKTOP-FCE7OUJ>
2022-09-12 13:09:59 +0200

    VirtualMethodsInConstructors

---
+++ CSharp/VirtualMethodsInConstructors/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+{
+    var derivée = new Derived();
+    Console.ReadLine();
+}
+
+
+public class Base
+{
+    public Base()
+    {
+        Init();
+    }
+
+    public virtual void Init()
+    {
+        Console.WriteLine("Base.Init");
+    }
+}
+
+public class Derived : Base
+{
+    private string s = "Non initialisée!";
+    public Derived()
+    {
+        s = "variable initialisée";
+    }
+
+    public override void Init()
+    {
+        Console.WriteLine("Derived.Init. var s = " + s);
+    }
+}
+
