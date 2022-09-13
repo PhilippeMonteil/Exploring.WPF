162db524830d0aa639609b2f97c8f017be4f5228
Philippe <Philippe@DESKTOP-FCE7OUJ>
2022-09-13 08:57:57 +0200

    update

---
+++ MVVM/TestMVVM0/ObservableObjectUser.cs
@@ -0,0 +1,33 @@
+﻿
+using CommunityToolkit.Mvvm.ComponentModel;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestMVVM0
+{
+
+    internal class ObservableObjectUser : ObservableObject
+    {
+        readonly User user;
+
+        public ObservableObjectUser(User user)
+        {
+            this.user = user;
+        }
+
+        public string Name
+        {
+            get => this.user.Name;
+            set
+            {
+                SetProperty(this.user.Name, value, user, (u, n) => u.Name = n);
+            }
+        }
+
+    }
+
+}
