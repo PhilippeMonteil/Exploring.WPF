ef4b627f6790812c180f1c435975ed35f83a90f7
Philippe <Philippe@DESKTOP-FCE7OUJ>
2022-09-15 09:01:51 +0200

    update

---
+++ WpfApp1/BehaviorTest.cs
@@ -0,0 +1,35 @@
+﻿
+using Microsoft.Xaml.Behaviors;
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfApp1
+{
+
+    public class BehaviorTest : Behavior<UIElement>
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            UIElement associatedObject = this.AssociatedObject; // <T>
+            Type associatedType = this.AssociatedType;
+            Debug.WriteLine($"{nameof(BehaviorTest)}.{nameof(OnAttached)}() associatedObject={associatedObject} associatedType={associatedType}");
+        }
+
+        protected override void OnDetaching()
+        {
+            DependencyObject associatedObject = this.AssociatedObject;
+            Type associatedType = this.AssociatedType;
+            Debug.WriteLine($"{nameof(BehaviorTest)}.{nameof(OnDetaching)}() associatedObject={associatedObject} associatedType={associatedType}");
+            base.OnDetaching();
+        }
+
+    }
+
+}
