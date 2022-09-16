7277a23baff0174d8d7d1803bfdd657fb632e945
Philippe <Philippe@DESKTOP-FCE7OUJ>
2022-09-16 16:05:14 +0200

    update

--- WpfApp1/App.xaml.cs
+++ WpfApp1/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -8,10 +9,21 @@ using System.Windows;
 
 namespace WpfApp1
 {
+
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application
     {
+        SplashScreen splashScreen;
+
+        private async void Application_Startup(object sender, StartupEventArgs e)
+        {
+            splashScreen = new SplashScreen("LeGeographe.PNG");
+            splashScreen.Show(false, true);
+            await Task.Delay(1000);
+            splashScreen.Close(TimeSpan.FromSeconds(1.0d));
+        }
     }
+
 }
