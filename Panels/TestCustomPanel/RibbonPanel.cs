dafdf99741f3acf38932b96811a927f522af4411
Philippe <Philippe@DESKTOP-FCE7OUJ>
2022-09-17 18:16:59 +0200

    update

--- Panels/TestCustomPanel/RibbonPanel.cs
+++ Panels/TestCustomPanel/RibbonPanel.cs
@@ -14,7 +14,28 @@ namespace TestCustomPanel
 
         protected override Size MeasureOverride(Size availableSize)
         {
-            int _im = this.Children.Count;
+            if (Children.Count <= 0) return new Size(0, 0);
+
+            UIElement _firstChild = (UIElement)Children[0];
+
+            _firstChild.Measure(new Size(double.PositiveInfinity, double.PositiveInfinity));
+
+            if (Children.Count == 1)
+            {
+                return _firstChild.DesiredSize;
+            }
+
+            double _child0_Width = _firstChild.DesiredSize.Width;
+
+            double _child_MaxWidth = 0;
+            for (int i = 1; i < Children.Count; i++)
+            {
+                Children[i].Measure(new Size(double.PositiveInfinity, double.PositiveInfinity));
+                if (_child_MaxWidth < Children[i].DesiredSize.Width)
+                {
+                    _child_MaxWidth = Children[i].DesiredSize.Width;
+                }
+            }
 
             foreach (UIElement child in Children)
             {
@@ -27,11 +48,6 @@ namespace TestCustomPanel
         protected override Size ArrangeOverride(Size finalSize)
         {
             int _im = this.Children.Count;
-
-            foreach (UIElement child in Children)
-            {
-                child.Arrange(new Rect(new Point(0, 0), finalSize));
-            }
 
             for (int i = 0; i < Children.Count; i++)
             {
