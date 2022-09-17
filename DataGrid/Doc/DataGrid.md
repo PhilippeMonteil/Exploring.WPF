f6454ce48374b1e6ceb7fbad1c560970cc211ca8
Philippe <Philippe@DESKTOP-FCE7OUJ>
2022-09-17 10:52:23 +0200

    upate

---
+++ DataGrid/Doc/DataGrid.md
@@ -0,0 +1,3 @@
+
+# [DataGrid Class](https://learn.microsoft.com/en-us/dotnet/api/system.windows.controls.datagrid?view=windowsdesktop-6.0)
+
---
+++ DataGrid/TestDataGrid/User.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestDataGrid
+{
+
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime Birthday { get; set; }
+
+        public string Details
+        {
+            get
+            {
+                return String.Format("{0} was born on {1} and this is a long description of the person.", this.Name, this.Birthday.ToLongDateString());
+            }
+        }
+    }
+
+}
